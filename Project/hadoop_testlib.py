5ad822c14d139e61267777130f7a71e0551d094d
Karan Bavishi <karan.bavishi90@gmail.com>
2016-10-29 15:14:17 -0500

    Adding more comments and docstrings for all functions in hadoop_testlib.py

--- Project/hadoop_testlib.py
+++ Project/hadoop_testlib.py
@@ -7,34 +7,50 @@ import spur
 from termcolor import cprint
 
 USERNAME = "kbavishi"
-#HADOOP_TAR_PATH = "https://archive.apache.org/dist/hadoop/common/hadoop-2.6.0/hadoop-2.6.0.tar.gz"
 HADOOP_TAR_PATH = "http://apache.cs.utah.edu/hadoop/common/hadoop-3.0.0-alpha1/hadoop-3.0.0-alpha1.tar.gz"
 
-# Issues I think I have resolved
-# 1. Passwordless login - Asks you for yes/no prompt
-# 2. Hosts file may be an issue
-# 3. There seems to be some NameNode process running on the topology at the
-# beginning
+# Issues that may still exist
+# 1. Hosts file may be an issue
 
 class TestShell(spur.SshShell):
     """
-    Wrapper class around the SshShell class. Provides easier way to run
-    commands by accepting strings.
+    Wrapper class around the spur.SshShell class. Provides the following
+    benefits:
 
-    Also keeps track of spawned processes and cleans them on object deletion.
-    This makes writing tests easier because you don't need cleanup code in each
-    test
+    1. Easier to run commands since it allows strings instead of just lists.
+    Eg. It allows "ls -al", compared to spur.SshShell which only allows
+    ["ls", "-al" ]
+
+    2. Also handles the pipe (|) and redirect operators (>>) in a cleaner way
+    than spur.SshShell
+
+    3. Prints commands in colour. Easier to distinguish between the command
+    being run and its output
+
+    4. Also keeps track of spawned processes and cleans them on object deletion.
+    [This doesn't quite work correctly yet]
     """
     def __init__(self, *args, **kwargs):
         self.processes = []
         super(TestShell, self).__init__(*args, **kwargs)
 
     def run_hadoop_cmd(self, cmd, *args, **kwargs):
+        """Use this function to run any command defined in run.sh.
+        Eg. start_all"""
         return self.run(["bash", "-c", "source run.sh; %s" % cmd],
                         *args, **kwargs)
 
     def run(self, cmd, *args, **kwargs):
-        if type(cmd) == str:
+        """Can run any bash command. Note:
+
+        1. To run complex bash commands with pipes and redirectors, set
+        "use_bash"=True in the kwargs just to be safe.
+        Eg. shell.run("ps aux | grep DataNode", use_bash=True)
+
+        2. Any commands defined in run.sh won't work. Use the function
+        `run_hadoop_cmd` instead for those commands.
+        """
+        if isinstance(cmd, str):
             if ">>" in cmd or "|" in cmd or 'use_bash' in kwargs:
                 # It is not possible to run commands which have pipes or the
                 # redirect operators without this hack
@@ -45,16 +61,27 @@ class TestShell(spur.SshShell):
         return super(TestShell, self).run(cmd, *args, **kwargs)
 
     def spawn(self, cmd, *args, **kwargs):
-        if type(cmd) == str:
+        if isinstance(cmd, str):
+            # Convert the `str` type command to type `list`
             cmd = shlex.split(cmd)
+
         if 'stdout' not in kwargs:
+            # Dump the output of the command to stdout. Without this, it is hard
+            # to know if a command is stuck
             kwargs['stdout'] = sys.stdout
+
+            # Print the command in yellow. This makes it easier to distinguish
+            # from its output. Useful if a lot of output is dumped
             cprint(" ".join(cmd), 'yellow', attrs=['bold'])
+
         proc = super(TestShell, self).spawn(cmd, *args, **kwargs)
         self.processes += [proc]
         return proc
 
-    def get_ip_addr(self):
+    def get_private_ip_addr(self):
+        """Gives you the private IP address which will be used for HDFS
+        communication. It assumes that they will be of the form 10.X.X.X"""
+
         result = self.run("hostname -I")
         ip_addrs = result.output.split(' ')
         # We know that CloudLab picks IP addrs of the form 10.x.x.x
@@ -65,30 +92,35 @@ class TestShell(spur.SshShell):
         assert False, "Could not find IP address"
 
     def __del__(self):
-        print 'HERE!!'
+        """Tries to cleanup any processes that may be running"""
         for process in self.processes:
             try:
                 process.send_signal(signal.SIGINT)
-                print 'KILLED proc SUCCESSFULLY'
             except:
                 # Process probably already dead
-                print 'FAILED to KILL proc'
                 continue
 
-def copyFile(shell, localPath, remotePath):
-    with shell.open(remotePath, "wcb") as remote_file:
-        with open(localPath, "rb") as local_file:
+def copy_file(shell, local_path, remote_path):
+    """Utility function to copy file present locally to the remote machine.
+    Specify the path of the file to be copied over in `local_path`, and its
+    destination path in `remote_path`"""
+
+    with shell.open(remote_path, "wcb") as remote_file:
+        with open(local_path, "rb") as local_file:
             shutil.copyfileobj(local_file, remote_file)
 
 def create_ssh_shell(hostname, username=USERNAME, password=None, port=22):
-    # Accept even if host key is missing
+    """Utility function which creates a TestShell class"""
+    # Accept even if host key is missing. Without this, it just fails and quits.
     return TestShell(hostname=hostname, username=username,
                      password=password, port=port,
                      missing_host_key=spur.ssh.MissingHostKey.accept)
 
 def install_dependencies(shell):
+    """Installs all packages that will be needed for deploying Hadoop"""
     try:
         # Check if packages have already been installed to save time
+        # We need Java 8 for running Hadoop 3.0.0-alpha1
         shell.run("dpkg -s openjdk-8-jdk")
         shell.run("dpkg -s pdsh")
     except:
@@ -100,6 +132,10 @@ def install_dependencies(shell):
         shell.run("sudo apt-get install -y pdsh")
 
 def create_hadoop_dirs(shell):
+    """Creates all logging and storage directories needed for running
+    applications like HDFS, Spark etc. in Hadoop"""
+
+    # Directories to be created
     dirs = ["conf", "logs", "software", "storage", "workload",
             "logs/apps", "logs/hadoop",
             "storage/data/local/nm", "storage/data/local/tmp",
@@ -110,19 +146,29 @@ def create_hadoop_dirs(shell):
     shell.run("mkdir -p %s" % " ".join(dirs))
 
 def setup_instances_file(shell, slave_ip_addrs):
+    """Creates a file named `instances` containing all the slave IP addresses.
+    This `instances` file is used by our `run.sh` script to start Hadoop daemons
+    on the slaves"""
     # XXX /etc/hosts may not be okay
+
+    # Delete the previous instances file. Without doing this, we risk adding
+    # duplicate entries to the file
     shell.run("rm instances", allow_error=True)
     shell.run("touch instances")
+
+    # Append all slave IP addrs to the file
     for slave_ip in slave_ip_addrs:
         shell.run("echo %s >> instances" % slave_ip)
     shell.run("cat instances")
 
 def setup_conf_tar(shell, master_ip):
-    # Create a tarball of conf files and send it over.
-    # After sending it over, substitute with the master IP
-    os.system("tar -cvzf conf.tar.gz conf/")
-    copyFile(shell, "conf.tar.gz", "conf.tar.gz")
+    """Copies over the XML conf files to the master"""
 
+    # Create a tarball of conf files available locally and send it over.
+    os.system("tar -cvzf conf.tar.gz conf/")
+    copy_file(shell, "conf.tar.gz", "conf.tar.gz")
+
+    # After sending it over, untar it and substitute with the master IP
     shell.run("tar -xvzf conf.tar.gz")
     for filename in shell.run("ls conf").output.split("\n"):
         if not filename:
@@ -131,21 +177,22 @@ def setup_conf_tar(shell, master_ip):
         shell.run("sed -i -e 's/MASTER_IP/%s/g' %s" % (master_ip, filepath))
 
 def setup_run_sh(shell):
-    # Deploy run.sh
-    copyFile(shell, "run.sh", "run.sh")
+    """Copies over the run.sh script needed for running Hadoop daemons"""
+    copy_file(shell, "run.sh", "run.sh")
     # NOTE: We need to update the SPARK_MASTER_IP if we intend to use Spark
 
 def setup_hadoop_tar(shell):
-    # We need the Hadoop tarball
+    """Downloads the Hadoop tarball and extracts it"""
     try:
         # The tarball is huge. Don't download if already present
         shell.run("ls hadoop-3.0.0-alpha1.tar.gz")
     except:
-        # Download the tarball
+        # Download the tarball since it doesn't exist
         shell.run("wget %s" % HADOOP_TAR_PATH)
         shell.run("tar -xzf hadoop-3.0.0-alpha1.tar.gz -C software")
 
 def kill_old_instances(shell):
+    """Kill any previously running Hadoop daemons"""
     # There are previously running instances of DataNode and NameNode if you
     # choose the Hadoop topology in CloudLab. Although we can use them directly,
     # our approach is more customizable. So kill those instances
@@ -156,23 +203,43 @@ def kill_old_instances(shell):
         pass
 
 def setup_hadoop(shell, master_ip, slave_ip_addrs):
+    """Sets up everything that is needed to run Hadoop on the cluster"""
+
+    # Kill any previously running daemons
     kill_old_instances(shell)
+
+    # Install all packages that will be needed. If they are already installed,
+    # the function will return quietly
     install_dependencies(shell)
+
+    # Create all directories needed for storing logs, container outputs etc.
     create_hadoop_dirs(shell)
+
+    # Setup instances file with slave IP addrs. This will be used by our scripts
+    # for running daemons on slave nodes
     setup_instances_file(shell, slave_ip_addrs)
+
+    # Copy over the XML configs
     setup_conf_tar(shell, master_ip)
+
+    # Copy over the run.sh script for running daemons
     setup_run_sh(shell)
+
+    # Download the Hadoop tarball if it doesn't exist
     setup_hadoop_tar(shell)
 
 def setup_passwordless(nn_shell, slave_shells):
+    """Sets up passwordless access between master and slave nodes. Needed for
+    running Hadoop daemons"""
     try:
         # Check if the RSA public key already exists
         nn_shell.run("ls .ssh/id_rsa.pub")
     except:
-        # Need to create one since nothing exists
+        # Need to create an RSA key since nothing exists
         nn_shell.run("ssh-keygen -f /users/kbavishi/.ssh/id_rsa -t rsa -N '' ",
                      use_bash=True)
 
+    # Add the public RSA key to the authorized_keys list on each slave
     publickey = nn_shell.run("cat .ssh/id_rsa.pub").output.strip("\n").strip()
     for slave_shell in slave_shells:
         try:
@@ -180,23 +247,33 @@ def setup_passwordless(nn_shell, slave_shells):
             slave_shell.run("grep '%s' .ssh/authorized_keys" % publickey,
                             use_bash=True)
         except:
+            # Hasn't been added. Update the authorized_keys list
             slave_shell.run("echo -e '%s' >> .ssh/authorized_keys" % publickey)
-            ip_addr = slave_shell.get_ip_addr()
+            ip_addr = slave_shell.get_private_ip_addr()
+            # Add the slave's IP address to the known_hosts list on the master.
+            # Creates issues otherwise
             nn_shell.run("ssh-keyscan -H %s >> .ssh/known_hosts" % ip_addr)
 
 def format_namenode(shell):
+    """Formats HDFS namenode. I believe this is for setting the namespace ID on
+    the NameNode which is then used in the handshakes between DataNodes and
+    NameNodes"""
     try:
-        # Formatting should be done only once. So check for indicator file
+        # Formatting should be done only once. So check for the presence of the
+        # file `formatting_done`.
         shell.run("ls formatting_done")
     except:
         shell.run_hadoop_cmd("hadoop namenode -format")
-        # Create indicator file so that we don't end up formatting again
+        # Create indicator file. Its presence will be used to ensure that we
+        # don't end up formatting again.
         shell.run("touch formatting_done")
 
 def run_TestDFSIO(shell, result_file="results.out", test_type="write",
                   number_of_files=1, file_size='1MB'):
     """
     Run TestDFSIO with various options. Assumes that start_all has been run
+    before. Check out the TestDFSIO documentation for more info about the test
+    parameters.
     """
     dfsio_jar = ("software/hadoop-3.0.0-alpha1/share/hadoop/mapreduce/"
                  "hadoop-mapreduce-client-jobclient-3.0.0-alpha1-tests.jar")
@@ -215,12 +292,17 @@ def save_output(output, filename):
     open(path, "w").write(output)
 
 def setup_hadoop_testbase(namenode, resourcemgr, slave0, slave1, slave2):
+    """Sets up everything needed for Hadoop to run on the cluster.
+    Returns the master shell for running testcases"""
+
     # Assume all entries are VMs and have ports embedded
+    # Create TestShell for the master
     nn_hostname, nn_port = namenode.split(':')
     nn_hostname += ".wisc.cloudlab.us"
     nn_shell = create_ssh_shell(nn_hostname, port=int(nn_port))
-    master_ip = nn_shell.get_ip_addr()
+    master_ip = nn_shell.get_private_ip_addr()
 
+    # Create TestShell for the resource manager
     rm_hostname, rm_port = resourcemgr.split(':')
     rm_hostname += ".wisc.cloudlab.us"
     rm_shell = create_ssh_shell(rm_hostname, port=int(rm_port))
@@ -228,30 +310,38 @@ def setup_hadoop_testbase(namenode, resourcemgr, slave0, slave1, slave2):
     # NOTE: We are not running a master and slave on the same instance
     slave_ip_addrs = []
 
+    # Create TestShell for the first slave
     slave0_hostname, slave0_port = slave0.split(':')
     slave0_hostname += ".wisc.cloudlab.us"
     slave0_shell = create_ssh_shell(slave0_hostname, port=int(slave0_port))
-    slave_ip_addrs += [slave0_shell.get_ip_addr()]
+    slave_ip_addrs += [slave0_shell.get_private_ip_addr()]
 
+    # Create TestShell for the second slave
     slave1_hostname, slave1_port = slave1.split(':')
     slave1_hostname += ".wisc.cloudlab.us"
     slave1_shell = create_ssh_shell(slave1_hostname, port=int(slave1_port))
-    slave_ip_addrs += [slave1_shell.get_ip_addr()]
+    slave_ip_addrs += [slave1_shell.get_private_ip_addr()]
 
+    # Create TestShell for the third slave
     slave2_hostname, slave2_port = slave2.split(':')
     slave2_hostname += ".wisc.cloudlab.us"
     slave2_shell = create_ssh_shell(slave2_hostname, port=int(slave2_port))
-    slave_ip_addrs += [slave2_shell.get_ip_addr()]
+    slave_ip_addrs += [slave2_shell.get_private_ip_addr()]
 
+    # Setup passwordless access between the master and each of the slave nodes
     setup_passwordless(nn_shell, [slave0_shell, slave1_shell, slave2_shell])
 
+    # Setup everything needed for running Hadoop on the cluster
     for shell in [nn_shell, rm_shell, slave0_shell,
                   slave1_shell, slave2_shell]:
         # XXX: Could be done in parallel
         setup_hadoop(shell, master_ip, slave_ip_addrs)
 
+    # Format the NameNode. This is needed only once
     format_namenode(nn_shell)
 
+    # Ensure that no previously running daemons exist. Note that before any test
+    # is run, "start all" has to be called
     nn_shell.run_hadoop_cmd("stop_all", allow_error=True)
 
     return nn_shell
